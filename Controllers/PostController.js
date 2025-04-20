eaeb318779803878d6032958e44c97b9a8e654e3
aniketsuryavanshi093 <aniketsuryavanshi093@gmail.com>
2025-04-21 00:11:10 +0530

    delete

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -60,18 +60,9 @@ export const updatePost = async (req, res) => {
 // delete a post
 export const deletePost = async (req, res) => {
     const id = req.params.id;
-
-    const { userId } = req.body;
-
     try {
-        const post = await postModel.findById(id);
-        if (post.userId === userId) {
-            await post.deleteOne();
-            res.status(200).json("Post deleted Successfully!")
-        } else {
-            res.status(403).json("Action forbidden")
-        }
-
+        await postModel.findByIdAndDelete(id);
+        res.status(200).json("Post deleted Successfully!")
     } catch (error) {
         res.status(500).json(error)
     }
